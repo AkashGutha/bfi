71437d42ebf0e7d4522617c8ba7785dbcbac3bc8
AkashGutha <akash.gutha@outlook.com>
2016-08-21 16:26:20 +0530

    added new modules to the build process

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,19 @@ var gulp = require('gulp');
 // Include helper libraries
 var uglify = require('gulp-uglify');
 var plumber = require('gulp-plumber');
-var ts = require('gulp-typescript');
-var typescript = require('typescript');
 var bump = require('gulp-bump');
 var del = require('del');
 var merge = require('merge2');
 var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
 var util = require('gulp-util');
-var browserify = require('gulp-browserify');
+var browserify = require('browserify');
 var header = require('gulp-header');
 var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var tsify = require('tsify');
+var ts = require('gulp-typescript');
+var typescript = require('typescript');
+var source = require('vinyl-source-stream');
 
 // create ts project
 var tsproject = ts.createProject('tsconfig.json');
@@ -35,16 +37,20 @@ var banner = ['/**',
 ].join('\n');
 
 var outFile = projectname + '-' + projectver + '.js';
+var outMinFile = projectname + '-' + projectver + '.min.js';
 
 // build task
 
 gulp.task('scripts', function() {
     var tsResult = gulp.src('src/**/*.ts')
         .pipe(plumber())
+        .pipe(sourcemaps.init())
         .pipe(ts(tsproject));
     return merge([
         tsResult.dts.pipe(gulp.dest('build/definitions')),
-        tsResult.js.pipe(gulp.dest('build/js'))
+        tsResult.js
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('build/js'))
     ]);
 });
 
@@ -58,31 +64,54 @@ gulp.task('scripts-onlycompile', function() {
 // concat and uglify
 
 gulp.task('concat', ['scripts'], function() {
-    gulp.src(['build/js/**/*.js', '!build/libraries/*'])
+    gulp.src(['build/js/**/*.js', '!lib/*'])
         .pipe(sourcemaps.init())
-        .pipe(concat(projectname + '-' + projectver + '.js'))
+        .pipe(concat(outFile))
         .on('error', util.log())
-        .pipe(gulp.dest('build/libraries'));
+        .pipe(gulp.dest('lib/'));
 });
 
 gulp.task('concat-force', function() {
-    gulp.src(['build/**/*.js', '!build/libraries/*'])
+    gulp.src(['build/**/*.js', '!lib/*'])
         .pipe(sourcemaps.init())
         .pipe(concat(projectname + '-' + projectver + '.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('build/libraries'));
+        .pipe(gulp.dest('lib/'));
 });
 
 // browserify task
 
-gulp.task('browserify', ['scripts'], function() {
-    gulp.src('build/js/main.js')
+gulp.task('browserify-sync', ['scripts'], function() {
+    browserify({
+            entries: './src/main.ts'
+        })
+        .plugin(tsify, { target: 'es5' })
+        .bundle()
         .pipe(plumber())
-        .pipe(browserify())
-        .pipe(uglify())
+        .pipe(source('outFile.js'))
         .pipe(header(banner, { pkg: pkg }))
         .pipe(rename(outFile))
-        .pipe(gulp.dest('build/libraries'));
+        .pipe(gulp.dest('lib/'));
+});
+
+//minify task
+
+gulp.task('minify-sync', ['browserify-sync'], function() {
+    gulp.src(['lib/*.js', '!lib/*.min.js'])
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(rename(outMinFile))
+        .pipe(gulp.dest('lib/'));
+});
+
+gulp.task('minify', function() {
+    gulp.src(['lib/*.js', '!lib/*.min.js'])
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(rename(outMinFile))
+        .pipe(gulp.dest('lib/'));
 });
 
 // watch task
@@ -97,17 +126,17 @@ gulp.task('watch-light', function() {
 
 // gulp delete task
 
-gulp.task('clean', ['browserify'], function() {
-    del(['!build/libraries/*', 'build/*']);
+gulp.task('clean-sync', ['minify-sync'], function() {
+    del('build/*');
 });
 
-gulp.task('clean-force', function() {
-    del(['!build/libraries/*', ['build/*']]);
+gulp.task('clean', function() {
+    del('build/*');
 });
 
 // default gulp task.
 
-gulp.task('default', ['scripts', 'browserify', 'clean']);
+gulp.task('default', ['scripts', 'browserify-sync', 'minify-sync']);
 
 // gulp tasks
 
