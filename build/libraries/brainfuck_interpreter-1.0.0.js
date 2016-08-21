2df8ad20c01cf2de489c1162aedd801b6c6565fe
AkashGutha <akash.gutha@outlook.com>
2016-08-21 16:27:30 +0530

    ignoring this files for now

--- build/libraries/brainfuck_interpreter-1.0.0.js
+++
@@ -1,7 +0,0 @@
-/**
- * brainfuck_interpreter - a brainfuck interpreter written in typescript
- * @version v1.0.0
- * @link www.akashgutha.com
- * @license MIT
- */
-!function r(e,n,t){function o(u,f){if(!n[u]){if(!e[u]){var c="function"==typeof require&&require;if(!f&&c)return c(u,!0);if(i)return i(u,!0);throw new Error("Cannot find module '"+u+"'")}var l=n[u]={exports:{}};e[u][0].call(l.exports,function(r){var n=e[u][1][r];return o(n?n:r)},l,l.exports,r,e,n,t)}return n[u].exports}for(var i="function"==typeof require&&require,u=0;u<t.length;u++)o(t[u]);return o}({1:[function(r,e,n){"use strict";function t(){console.log("hi Welcome to BFI!")}t()},{}]},{},[1]);
